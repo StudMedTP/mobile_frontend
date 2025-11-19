98cc4abf01765f2d548ef825382616a76b6e602f
JoseR200 <joserodrigolopez@icloud.com>
2025-11-20 00:20:56 +0100

    fix: test in microservice

--- lib/pages/data/http_helper.dart
+++ lib/pages/data/http_helper.dart
@@ -5,15 +5,19 @@ import 'package:http/http.dart' as http;
 
 class HttpHelper {
 
-  final String urlBase = 'https://studmed.aurumtech.site/api-gateway';
+  final String urlBase = 'https://studmed.aurumtech.site/user';
   
 
   Future<Map<String, dynamic>> login(String email, String password, String role) async {
       http.Response response = await http.post(
-          Uri.parse('$urlBase/microservice-user/users/login'), body: {
+          Uri.parse('$urlBase/users/login'),
+          headers: {
+              'Content-Type': 'application/json',
+          },
+          body: json.encode({
               "email": email,
               "password": password,
-          }
+          })
       );
       try {
           final Map<String, dynamic> jsonResponse = json.decode(response.body);
